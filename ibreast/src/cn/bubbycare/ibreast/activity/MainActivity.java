264b489d5d99bcd65f5bb1e9c482d8d799934794
xushiyong <835588741@qq.com>
2015-07-02 00:52:06 +0800

    创建主类提交

---
+++ ibreast/src/cn/bubbycare/ibreast/activity/MainActivity.java
@@ -0,0 +1,28 @@
+/**
+ * 文件名：MainActivity.java 2015-7-2 上午12:47:05
+ * @author Administrator 	许仕永
+ */
+package cn.bubbycare.ibreast.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**Description : 应用主界面，承载登录注册页面的Activity
+ * Create time : 2015-7-2 上午12:47:05
+ * Project name: ibreast
+ * File name   : MainActivity.java
+ * Encoded     : UTF-8
+ * @author     许仕永
+ * @JKD        JDK 1.6.0_21 
+ * @version    v1.0.0
+ */
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+}
